91c4ee9b9ea0abfd8092d45d9c584bf462d5f521
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-12-01 14:51:34 +0100

    Update functions part1 and part2 in src/Day01.kt
    Updated the calculation logic in src/Day01.kt. In part1 function, input data is now transformed by concatenating first and last digits of each value, then their sum is returned. In part2 function, the return value is now a sum of concatenated first and last digits extracted from both numerical and written formats. These adjustments provide more accurate calculation results for the given task.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,60 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        input.forEach{ it ->
+            val number = it.filter { it.isDigit() }
+            val lastDigit = (number.toDouble() % 10).toInt()
+            val firstDigit = number.first().toString().toInt()
+
+
+            val numberFirstLast = (firstDigit.toString() + lastDigit.toString()).toInt()
+
+            result += numberFirstLast
+
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val DIGIT_WORDS = mapOf(
+            "one"       to 1,
+            "two"       to 2,
+            "three"     to 3,
+            "four"      to 4,
+            "five"      to 5,
+            "six"       to 6,
+            "seven"     to 7,
+            "eight"     to 8,
+            "nine"      to 9,
+        )
+
+        val startingLetters = DIGIT_WORDS.keys.map { it.first() }.toSet()
+
+        return input.sumOf { line ->
+            val lineNumbers = line.mapIndexed { idx, c ->
+                if (c.isDigit()) {
+                    c - '0'
+                } else if (startingLetters.contains(c)) {
+                    DIGIT_WORDS.entries.firstOrNull { (word, digit) ->
+                        word == line.substring(idx, Math.min(idx + word.length, line.length))
+                    }.let { it?.value } // do this better
+                } else {
+                    null
+                }
+            }.filterNotNull()
+
+            val concatNumber = "${lineNumbers.first()}${lineNumbers.last()}".toInt()
+            concatNumber
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01")
+    //part1(testInput).println()
+    part2(testInput).println()
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("Day01_test")
+    //part1(input).println()
+    //part2(input).println()
 }
